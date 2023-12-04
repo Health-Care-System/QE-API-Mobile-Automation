1c9486477ad0414451c2d74b0615133c45cf2611
AndreanRivai <fernandandreanrivai@gmail.com>
2023-12-04 17:07:51 +0700

    create step class and step definition for articles

--- src/test/java/starter/stepdefinitions/ArticlesSteps.java
+++ src/test/java/starter/stepdefinitions/ArticlesSteps.java
@@ -1,5 +1,6 @@
 package starter.stepdefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -10,5 +11,128 @@ public class ArticlesSteps {
 
     @Steps
     Articles articles;
+
+
+
+    //    Scenario: Verify send GET request to get all health articles endpoint
+    @Given("I set get all health articles API endpoint")
+    public void setValidGetAllHealthArticlesEndpoint(){
+        articles.setValidGetAllHealthArticles();
+    }
+
+    @When("I send GET HTTP request for get all health articles")
+    public void successSendGetAllHealthArticles(){
+        articles.sendValidGetAllHealthArticles();
+    }
+
+    @And("I receive valid message that successfully get data article")
+    public void receiveMessageSuccessfullyGetDataArticle(){
+        articles.validateMessageSuccessGetAllHealthArticles();
+    }
+
+
+
+    //    Scenario: Verify send POST request to get all health articles endpoint
+    @When("I send POST HTTP request for get all health articles")
+    public void successSendPostToGetAllHealthArticles(){
+        articles.sendPostToGetAllHealthArticles();
+    }
+
+
+    //    Scenario: Verify send GET request to get health article by ID endpoint with valid article ID
+    @Given("I set get health article by ID API endpoint with valid ID")
+    public void setValidGetArticleByIDEndpoint(){
+        articles.setValidGetArticleByID();
+    }
+
+    @When("I send GET HTTP request for get health article by valid ID")
+    public void successSendGetHealthArticleByID(){
+        articles.sendValidGetHealthArticleByID();
+    }
+
+    @And("I receive valid message that successfully get data")
+    public void receiveMessageSuccessfullyGetData(){
+        articles.validateMessageSuccessGetArticleByID();
+    }
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with invalid article ID
+    @Given("I set get health article by invalid ID API endpoint with invalid ID")
+    public void setGetArticleByInvalidIDEndpoint(){
+        articles.setGetArticleByInvalidID();
+    }
+
+    @When("I send GET HTTP request for get health article by invalid ID")
+    public void successSendGetArticleByInvalidID(){
+        articles.sendGetHealthArticleByInvalidID();
+    }
+
+    @And("I receive valid message that invalid id param")
+    public void receiveMessageInvalidIDParam(){
+        articles.validateMessageGetArticleByInvalidID();
+    }
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with non existing article ID
+    @Given("I set get health article by ID API endpoint with non existing ID")
+    public void setGetHealthArticleByNonExistID(){
+        articles.setGetArticleByNonExistID();
+    }
+
+    @When("I send GET HTTP request for get health article non existing ID")
+    public void successSendGetHealthArticleByNonExistID(){
+        articles.sendGetHealthArticleByNonExistID();
+    }
+
+    @And("I receive valid message that not found")
+    public void receiveMessageArticleNotFound(){
+        articles.validateMessageGetNonExistArticleID();
+    }
+
+
+
+//    Scenario: Verify send POST request to get health article by ID endpoint with valid article ID
+    @When("I send POST HTTP request for get health article by valid ID")
+    public void successSendPostToGetHealthArticleByID(){
+        articles.sendPostToGetHealthArticleByID();
+    }
+
+
+    //    Scenario: Verify send GET request to get health article by ID endpoint with valid article title
+    @Given("I set get health article by title API endpoint with valid title")
+    public void setValidGetArticleByTitle(){
+        articles.setValidGetHealthArticleByTittle();
+    }
+
+    @When("I send GET HTTP request for get health article by valid title")
+    public void successSendGetHealthArticleByTittle(){
+        articles.sendValidGetArticleByTittle();
+    }
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with invalid article title
+    @Given("I set get health article by title API endpoint with invalid title")
+    public void setGetArticleByInvalidTitle(){
+        articles.setGetHealthArticleByInvalidTitle();
+    }
+
+    @When("I send GET HTTP request for get health article by invalid title")
+    public void successSendGetArticleByInvalidTitle(){
+        articles.sendGetHealthArticleByInvalidTitle();
+    }
+
+
+    //    Scenario: Verify send GET request to get health article by ID endpoint with empty parameter
+    @And("I receive valid message that title query param required")
+    public void receiveMessageTitleQueryParamRequired(){
+        articles.validateMessageEmptyTitle();
+    }
+
+
+    //    Scenario: Verify send POST request to get health article by ID endpoint with valid article title
+    @When("I send POST HTTP request for get health article by valid title")
+    public void successSendPostToGetArticleByValidTitle(){
+        articles.sendPostToGetHealthArticleByTittle();
+    }
 
 }
--- src/test/java/starter/user/Articles.java
+++ src/test/java/starter/user/Articles.java
@@ -1,11 +1,142 @@
 package starter.user;
 
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import net.serenitybdd.rest.SerenityRest;
 import net.thucydides.core.annotations.Step;
 import org.json.JSONObject;
 import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class Articles {
 
     protected String url = "";
+
+//    Scenario: Verify send GET request to get all health articles endpoint
+    @Step("I set get all health articles API endpoint")
+    public  String setValidGetAllHealthArticles(){
+        return url + "/users/articles";
+    }
+
+    @Step("I send GET HTTP request for get all health articles")
+    public void sendValidGetAllHealthArticles(){
+        SerenityRest.given().get(setValidGetAllHealthArticles());
+    }
+
+    @Step("I receive valid message that successfully get data article")
+    public void validateMessageSuccessGetAllHealthArticles(){
+        restAssuredThat(response -> response.body("'meta'.'message'", equalTo("successfully get data article")));
+    }
+
+
+//    Scenario: Verify send POST request to get all health articles endpoint
+    @Step("I send POST HTTP request for get all health articles")
+    public void sendPostToGetAllHealthArticles(){
+        SerenityRest.given().post(setValidGetAllHealthArticles());
+    }
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with valid article ID
+    @Step("I set get health article by ID API endpoint with valid ID")
+    public String setValidGetArticleByID(){
+        return url + "/users/articles/7";
+    }
+
+    @Step("I send GET HTTP request for get health article by valid ID")
+    public void sendValidGetHealthArticleByID(){
+        SerenityRest.given().post(setValidGetArticleByID());
+    }
+
+    @Step("I receive valid message that successfully get data")
+    public void validateMessageSuccessGetArticleByID(){
+        restAssuredThat(response -> response.body("'meta'.'message'", equalTo("successfully get data ")));
+    }
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with invalid article ID
+    @Step("I set get health article by invalid ID API endpoint with invalid ID")
+    public String setGetArticleByInvalidID(){
+        return url + "/users/articles/abcde";
+    }
+
+    @Step("I send GET HTTP request for get health article by invalid ID")
+    public void sendGetHealthArticleByInvalidID(){
+        SerenityRest.given().get(setGetArticleByInvalidID());
+    }
+
+    @Step("I receive valid message that invalid id param")
+    public void validateMessageGetArticleByInvalidID(){
+        restAssuredThat(response -> response.body("'meta'.'message'", equalTo("invalid id param")));
+    }
+
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with non existing article ID
+    @Step("I set get health article by ID API endpoint with non existing ID")
+    public String setGetArticleByNonExistID(){
+        return url + "/users/articles/999";
+    }
+
+    @Step("I send GET HTTP request for get health article non existing ID")
+    public void sendGetHealthArticleByNonExistID(){
+        SerenityRest.given().get(setGetArticleByNonExistID());
+    }
+
+    @Step("I receive valid message that not found")
+    public void validateMessageGetNonExistArticleID(){
+        restAssuredThat(response -> response.body("'meta'.'message'", equalTo("not found")));
+    }
+
+
+//    Scenario: Verify send POST request to get health article by ID endpoint with valid article ID
+    @Step("I send POST HTTP request for get health article by valid ID")
+    public void sendPostToGetHealthArticleByID(){
+        SerenityRest.given().post(setValidGetArticleByID());
+    }
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with valid article title
+    @Step("I set get health article by title API endpoint with valid title")
+    public String setValidGetHealthArticleByTittle(){
+        return url + "/users/article?title=mata";
+    }
+
+    @Step("I send GET HTTP request for get health article by valid title")
+    public void sendValidGetArticleByTittle(){
+        SerenityRest.given().get(setValidGetHealthArticleByTittle());
+    }
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with invalid article title
+    @Step("I set get health article by title API endpoint with invalid title")
+    public String setGetHealthArticleByInvalidTitle(){
+        return url + "/users/article?title=askasaks";
+    }
+
+    @Step("I send GET HTTP request for get health article by invalid title")
+    public void sendGetHealthArticleByInvalidTitle(){
+        SerenityRest.given().get(setGetHealthArticleByInvalidTitle());
+    }
+
+
+
+//    Scenario: Verify send GET request to get health article by ID endpoint with empty parameter
+    @Step("I receive valid message that title query param required")
+    public void validateMessageEmptyTitle(){
+        restAssuredThat(response -> response.body("'meta'.'message'", equalTo("title query param required")));
+    }
+
+
+
+//    Scenario: Verify send POST request to get health article by ID endpoint with valid article title
+    @Step("I send POST HTTP request for get health article by valid title")
+    public void sendPostToGetHealthArticleByTittle(){
+        SerenityRest.given().post(setValidGetHealthArticleByTittle());
+    }
+
+
+
+
 
 }
